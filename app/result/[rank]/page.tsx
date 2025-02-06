9ca2d9234b6f6c59e6ed3119b25ebaee6fe4cdbe
sohaekim <sohaekim1@gmail.com>
2025-02-06 20:40:38 +0900

    update: 결과 등급 수정

--- app/result/[rank]/page.tsx
+++ app/result/[rank]/page.tsx
@@ -3,69 +3,50 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import ShareButton from "@/components/ShareButton";
 
+export type Rank = "bald" | "downy" | "grass" | "plant" | "jungle";
+
 type Params = Promise<{
-  rank: string;
+  rank: Rank;
 }>;
 
 export async function generateStaticParams() {
   return [
-    { rank: "1" },
-    { rank: "2" },
-    { rank: "3" },
-    { rank: "4" },
-    { rank: "5" },
+    { rank: "bald" },
+    { rank: "downy" },
+    { rank: "grass" },
+    { rank: "plant" },
+    { rank: "jungle" },
   ];
 }
 
 const RESULT_MAP: {
-  [key in `rank${string}`]: { desc: string; title: string };
+  [key in Rank]: { desc: string; title: string };
 } = {
-  rank1: {
+  bald: {
     desc: "경제 상식? 그런 건 머리에서 반짝거리기만 해요!",
     title: "광택나는 머머리",
   },
-  rank2: {
+  downy: {
     desc: "바람 불면 머리카락이 날아갈지 몰라요.",
     title: "희미한 솜털머리",
   },
-  rank3: {
+  grass: {
     desc: "경제지식의 새싹이 나고 있습니다!",
     title: "열심히 심은 새싹머리",
   },
-  rank4: {
+  plant: {
     desc: "지식이 무르익었네요. 추수의 계절이 다가오고 있습니다!",
     title: "황금빛 벼머리",
   },
-  rank5: {
+  jungle: {
     title: "수풀 가득한 정글머리",
     desc: "머리가 이렇게 풍성할 수 있나요? 대단합니다!",
   },
 };
 
-const RESULT = [
-  {
-    id: " 그런 건 머리에서 반짝거리기만 해요!",
-    title: "광택나는 머머리",
-  },
-  {
-    id: 2,
-    desc: "바람 불면 머리카락이 날아갈지 몰라요.",
-    title: "희미한 솜털머리",
-  },
-  {
-    id: 3,
-    desc: "경제지식의 새싹이 나고 있습니다!",
-    title: "열심히 심은 새싹머리",
-  },
-  {
-    id: 4,
-    desc: "경제상식? 그런 건 머리에서 반짝거리기만 해요!",
-    title: "광택나는 머머리",
-  },
-];
 export default async function Page({ params }: { params: Params }) {
   const { rank } = await params;
-  const { title, desc } = RESULT_MAP[`rank${rank}`];
+  const { title, desc } = RESULT_MAP[`${rank}`];
 
   return (
     <div>
--- components/QuizOptions.tsx
+++ components/QuizOptions.tsx
@@ -1,37 +1,49 @@
 "use client";
-import React, { Dispatch, SetStateAction } from "react";
+import React from "react";
 import { Button } from "@/components/ui/button";
-import { usePathname, useRouter } from "next/navigation";
-import { Quiz, QuizOption } from "@/app/test/page";
+import { useRouter } from "next/navigation";
+import { QuizOption } from "@/app/test/page";
 import { useUser } from "@/state/UserContext";
-import PreviousMap from "postcss/lib/previous-map";
-
+import { Rank } from "@/app/result/[rank]/page";
 interface Props {
   options: QuizOption[];
   isLastQuiz: boolean;
 }
 
-function getResult() {}
+function getRank(score: number): Rank {
+  if (score <= 2) {
+    return "bald";
+  } else if (score <= 4) {
+    return "downy";
+  } else if (score <= 6) {
+    return "grass";
+  } else if (score <= 8) {
+    return "plant";
+  } else if (score <= 10) {
+    return "jungle";
+  }
+  return "bald";
+}
 
 export default function QuizOptions({ options, isLastQuiz }: Props) {
   const { replace } = useRouter();
   const {
-    user: { progress },
+    user: { progress, score },
     setUser,
   } = useUser();
 
   const onClickButton = (option: QuizOption) => {
     // [1] 점수, 진행도 기록: 맞으면 득점
-    const newScore = option.isCorrect ? 1 : 0;
-    setUser((prev) => ({
-      ...prev,
-      progress: prev.progress + (isLastQuiz ? 0 : 1),
-      score: prev.score + newScore,
-    }));
+    const newScore = score + (option.isCorrect ? 1 : 0);
+    setUser({
+      progress: progress + (isLastQuiz ? 0 : 1),
+      score: newScore,
+    });
 
     // [2] 마지막 문항일 경우, 결과 페이지로 이동
     if (isLastQuiz) {
-      replace("/result/1");
+      const rank = getRank(newScore);
+      replace(`/result/${rank}`);
     }
   };
 
@@ -40,7 +52,7 @@ export default function QuizOptions({ options, isLastQuiz }: Props) {
       {options.map((option) => (
         <Button
           variant="outline"
-          size="lg"
+          size="md"
           key={`${progress}_${option.id}`}
           onClick={() => onClickButton(option)}
         >
