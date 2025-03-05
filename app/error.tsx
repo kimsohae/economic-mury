7834f6555b0762147801428aa16a53dec852da1d
sohaekim <sohaekim1@gmail.com>
2025-03-05 20:31:49 +0900

    update: 에러페이지 처음으로 버튼

--- app/error.tsx
+++ app/error.tsx
@@ -1,6 +1,17 @@
 "use client";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import React from "react";
 
 export default function error() {
-  return <div>error</div>;
+  return (
+    <div className="flex flex-col gap-4">
+      <span>에러가 발생했어요</span>
+      <Link href="/">
+        <Button size="md" variant={"default"} className="text-white">
+          처음으로
+        </Button>
+      </Link>
+    </div>
+  );
 }
